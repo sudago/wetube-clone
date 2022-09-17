9bc39556a47f6d2c833af3be7b6f143f0dcfeaa4
sudago <sudago22@gmail.com>
2022-09-17 17:06:44 +0900

    use .env
    protect APIs.

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,16 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/userController";
+import { getEdit, postEdit, logout, see, startGithubLogin, finishGithubLogin, getChangePassword, postChangePassword } from "../controllers/userController";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
-userRouter.get("/remove", remove);
+// protectorMiddleware = 로그인 되어있어야 접근 가능.
+// publicOnlyMiddleware = 로그아웃 되어있어야 접근 가능.
+userRouter.get("/logout", protectorMiddleware, logout);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter.route("/change-password").all(protectorMiddleware).get(getChangePassword).post(postChangePassword);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get(":id", see);
 
 export default userRouter;
